7e336bd70ed4e649a31da215dd27cf6ae71299d6
full-st <167549821+full-st@users.noreply.github.com>
2024-05-30 17:47:32 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import logo from './assets/logo.svg'
+import {FaBars} from 'react-icons/fa'
+import { links,social } from './data'
+import { useRef, useState } from 'react'
+const App = () => {
+  const [showLinks,setShowLinks] = useState(false)
+  const refContainer = useRef(null)
+  const linksRef = useRef(null)
+  const linkStyles = {
+    height:showLinks?`${linksRef.current.getBoundingClientRect().height}px`:'0px'
+  }
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className='logo' alt='logo'/>
+          <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}><FaBars/></button>
+        </div>
+        <div className="links-container" ref={refContainer} style={linkStyles}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link)=>{
+              const {id,url,text} = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon)=>{
+            const {id,url,icon} = socialIcon
+            return(
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default App
